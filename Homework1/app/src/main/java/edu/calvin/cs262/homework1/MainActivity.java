7ea804839996d28f7d4570aed60ab66ba3f89ba9
stz4 <stz4@maroon04.cs.calvin.edu>
2019-11-15 20:57:11 -0500

    added comments

--- Homework1/app/src/main/java/edu/calvin/cs262/homework1/MainActivity.java
+++ Homework1/app/src/main/java/edu/calvin/cs262/homework1/MainActivity.java
@@ -1,41 +1,42 @@
 package edu.calvin.cs262.homework1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.util.Log;
 
-import org.w3c.dom.Text;
 /**
-  *Creates a main activity
-  *@Author: Sebrina Zeleke
+ * Creates a main activity
+ *
+ * @Author: Sebrina Zeleke
  */
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spinner;
-    ArrayAdapter<CharSequence> operator;
-    int selected_item;
+    private Spinner spinner;
+    private int selected_item;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayAdapter<CharSequence> operator;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         spinner = findViewById(R.id.spinner);
-        operator = ArrayAdapter.createFromResource(this,R.array.operators, android.R.layout.simple_spinner_item);
+        operator = ArrayAdapter.createFromResource(this, R.array.operators, android.R.layout.simple_spinner_item);
         operator.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(operator);
         selected_item = spinner.getSelectedItemPosition();
     }
 
     /**
-    * This method gets called when the calculate button is called
-    * @param: View
-    * @return void
+     * This method gets called when the calculate button is called
+     *
+     * @return void
+     * @param: View
      */
     public void calculate_operation(View view) {
         //get the first number
@@ -53,19 +54,16 @@ public class MainActivity extends AppCompatActivity {
 
         //calculate the result based on the operator provided and the number given
 
-        if(selected_item == 0){
+        if (selected_item == 0) {
             int add_num = Integer.parseInt(first_entry) + Integer.parseInt(second_entry);
             final_result.setText(String.valueOf(add_num));
-        }
-        else if(selected_item == 1){
+        } else if (selected_item == 1) {
             int sub_num = Integer.parseInt(first_entry) - Integer.parseInt(second_entry);
             final_result.setText(String.valueOf(sub_num));
-        }
-        else if(selected_item == 2){
+        } else if (selected_item == 2) {
             int mul_num = Integer.parseInt(first_entry) / Integer.parseInt(second_entry);
             final_result.setText(String.valueOf(mul_num));
-        }
-        else if(selected_item == 3){
+        } else if (selected_item == 3) {
             int div_num = Integer.parseInt(first_entry) * Integer.parseInt(second_entry);
             final_result.setText(String.valueOf(div_num));
         }
